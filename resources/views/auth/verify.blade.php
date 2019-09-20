a8681aa2af369f494e9e966486970600067dae69
Armando Claudio Jimenez <claudiojimenez.ar@gmail.com>
2019-09-19 21:45:26 -0400

    Upgrade to Laravel 6.0

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -21,9 +21,13 @@
                             {{ __('Before proceeding, please check your email for a verification link.') }}
                         </p>
 
-                        <a class="bg-blue-500 hover:bg-blue-700 text-gray-100 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline no-underline mx-auto" href="{{ route('verification.resend') }}">
-                            {{ __('Resend verification email') }}
-                        </a>
+                        <form method="POST" action="{{ route('verification.resend') }}">
+                            @csrf
+
+                            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-gray-100 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline no-underline mx-auto">
+                                {{ __('Resend verification email') }}
+                            </button>.
+                        </form>
                     </div>
 
                 </div>
